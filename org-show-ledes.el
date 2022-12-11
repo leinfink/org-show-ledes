7a13913caa10fa61e0e87c12b6181c0d5112f2ab
Henrik Hörmann <leinfink@mailbox.org>
2022-12-11 14:58:19 +0100

    switched arg and non-arg actions

--- org-show-ledes.el
+++ org-show-ledes.el
@@ -1,16 +1,16 @@
 (defun org-show-ledes (&optional arg)
   "Show all headings and the first paragraph in each body.
-If ARG is non-nil, use an empty line as paragraph delimiter.
-If ARG is omitted or nil, use the first linebreak."
+If ARG is omitted or nil, use an empty line as paragraph delimiter.
+If ARG is non-nil, use the first linebreak."
   (interactive "P")
   (org-show-all '(headings drawers))
   (save-excursion
     (goto-char (point-max))
-    (let ((regexp (if arg "^\*+.*\\(?:\n.*\\)*?\n\n" "^\*+.*\n.*"))
+    (let ((regexp (if arg  "^\*+.*\n.*" "^\*+.*\\(?:\n.*\\)*?\n\n"))
           (last (point)))
       (while (re-search-backward regexp nil 't)
         (goto-char (match-end 0))
-        (unless arg (forward-line))
+        (if arg (forward-line))
         (org-flag-region (point) last t 'outline)
         (goto-char (match-beginning 0))
         (forward-line -1)
